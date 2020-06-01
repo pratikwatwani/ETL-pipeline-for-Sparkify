910d39a0362f926ba0e9e33f7876ab1640783054
Pratik Watwani <pratikpwatwani@gmail.com>
2020-05-31 21:58:51 -0400

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -15,17 +15,17 @@ songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users
-                (user_id int, first_name varchar, last_name varchar,\
+                (user_id int PRIMARY KEY, first_name varchar, last_name varchar,\
                 gender varchar(1), level varchar);
 """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs
-                (song_id varchar, title varchar, artist_id varchar,\
+                (song_id varchar PRIMARY KEY, title varchar, artist_id varchar,\
                 year int, duration numeric);
 """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
-                (artist_id varchar, name varchar, location varchar,\
+                (artist_id varchar PRIMARY KEY, name varchar, location varchar,\
                 latitude numeric, longitude numeric);
 """)
 
@@ -43,14 +43,17 @@ songplay_table_insert = ("""INSERT INTO songplays ( start_time, user_id, level,\
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)\
                 VALUES (%s, %s, %s, %s, %s)
+                ON CONFLICT (user_id) DO NOTHING
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year,duration)\
                 VALUES (%s, %s, %s, %s, %s)
+                ON CONFLICT (song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)\
                 VALUES (%s, %s, %s, %s, %s)
+                ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
